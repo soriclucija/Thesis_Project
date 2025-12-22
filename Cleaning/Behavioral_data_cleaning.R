8a0ff55c00511daa8bb4eefeb1da2557c586194a
Lucija <lucijaxsoric@gmail.com>
2025-12-22 14:28:37 +0100

    Update Behavioral_data_cleaning.R
    Updated the data cleaning function to handle header rows and filter trial data correctly.

--- Cleaning/Behavioral_data_cleaning.R
+++ Cleaning/Behavioral_data_cleaning.R
@@ -5,17 +5,20 @@ library(purrr)
 input_folder <- "C:/Users/lucij/Desktop/Leiden/Year 2/Thesis Project/2024_data/raw_behavioral_data/"
 output_folder <- "C:/Users/lucij/Desktop/Leiden/Year 2/Thesis Project/2024_data/cleaned_behavioral_data/"
 
-
-if(!dir.exists(output_folder)) dir.create(output_folder, recursive = TRUE)
+if (!dir.exists(output_folder)) dir.create(output_folder, recursive = TRUE)
 
 files <- list.files(input_folder, pattern = "\\.csv$", full.names = TRUE)
 
 clean_file <- function(file_path) {
+  
   data <- read_csv(file_path, trim_ws = TRUE)
   
   first_value <- function(x) x[which(!is.na(x) & x != "")][1]
   
-  repeat_values <- data[1:20, ] %>%
+  header_rows <- data %>%
+    filter(is.na(trials.thisTrialN))
+  
+  repeat_values <- header_rows %>%
     summarise(
       instructions = first_value(instructions),
       age_slider.response = first_value(age_slider.response),
@@ -24,7 +27,8 @@ clean_file <- function(file_path) {
       session_start = first_value(session_start)
     )
   
-  trial_data <- data[-c(1:20), ] %>%
+  trial_data <- data %>%
+    filter(!is.na(trials.thisTrialN)) %>%
     mutate(
       instructions = repeat_values$instructions,
       age_slider.response = repeat_values$age_slider.response,
@@ -43,21 +47,28 @@ clean_file <- function(file_path) {
     "trials.thisIndex", "thisRow.t", "signed_contrast",
     "leftCont", "rightCont", "reaction_time", "response_time",
     "correct", "timeout",
-    "instructions", "age_slider.response", "mouse_5.clicked_name", "mouse_6.clicked_name"
+    "instructions", "age_slider.response",
+    "mouse_5.clicked_name", "mouse_6.clicked_name"
   )
   
-  cleaned <- trial_data %>% select(all_of(final_cols)) %>% slice(1:600)
+  cleaned <- trial_data %>%
+    select(all_of(final_cols)) %>%
+    slice_head(n = 600)
   
   file_name <- basename(file_path)
-  output_file <- file.path(output_folder, gsub("\\.csv$", "_cleaned.csv", file_name))
+  output_file <- file.path(
+    output_folder,
+    gsub("\\.csv$", "_cleaned.csv", file_name)
+  )
   
   write_csv(cleaned, output_file)
   
   return(output_file)
 }
 
-
 cleaned_files <- map(files, clean_file)
 cleaned_files
+
+
 
 
